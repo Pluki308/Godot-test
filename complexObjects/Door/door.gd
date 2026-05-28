b65436dbbec6f47d4d165ed71bb8f1d85c7d719e
Pluki308 <pluk.vddonker@gmail.com>
2026-05-28 15:36:32 +0200

    niks eigenlijk

--- complexObjects/Door/door.gd
+++ complexObjects/Door/door.gd
@@ -1,15 +1,16 @@
 extends Node3D
 
 @export var closed = true
+@export var interactable = true
 @export var animation_player: AnimationPlayer
 
 
 func interact():
-	
-	closed = !closed
-	if closed == true:
-		animation_player.play("open")
-	else:
-		animation_player.play_backwards("open")
-		
+	if interactable:
+		closed = !closed
+		if closed == true:
+			animation_player.play("open")
+		else:
+			animation_player.play_backwards("open")
+			
 	
